133273b57c429e4fd9f70f0183a0b3b4f55a665c
Mark Haylock <mh@trineo.co.nz>
2021-07-13 22:00:40 +1200

    Add movie rendering and pagination

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,38 @@
 class MoviesController < ApplicationController
+  helper_method :previous_page_path, :next_page_path
+
+  def index
+    @movies = Movie
+      .order('start_year DESC NULLS LAST, id ASC')
+      .limit(per_page)
+      .offset(offset)
+
+    start = Time.now
+    @movies.load
+    @query_time = Time.now - start
+  end
+
+  protected
+
+  def previous_page_path
+    movies_path(page: page - 1, per_page: per_page) if page > 1
+  end
+
+  def next_page_path
+    movies_path(page: page + 1, per_page: per_page)
+  end
+
+  private
+
+  def page
+    (params[:page] || '1').to_i
+  end
+
+  def offset
+    (page - 1) * per_page
+  end
+
+  def per_page
+    (params[:per_page] || '500').to_i
+  end
 end
